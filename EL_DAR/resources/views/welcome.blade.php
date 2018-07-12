260be622b2ba7904b8074bf40419e36de405a211
mostafasaleh44 <mostafa.saleh423@yahoo.com>
2018-07-12 07:48:54 -1000

    trymerg

--- EL_DAR/resources/views/welcome.blade.php
+++ EL_DAR/resources/views/welcome.blade.php
@@ -5,20 +5,26 @@
 
         <div class="uk-navbar-center-left uk-light uk-background-secondary"><div>
             <ul class="uk-navbar-nav">
-                <li class="uk-active"><a href="#">About</a></li>
+                <li ><a href="#">About</a></li>
                 <li>
-                    <a href="#">typs</a>
+                    <a href="#">modern</a>
                     <div class="uk-navbar-dropdown uk-light uk-background-secondary">
                         <ul class="uk-nav uk-navbar-dropdown-nav">
-                            <li class="uk-active"><a href="#">Active</a></li>
-                            <li><a href="#">Item</a></li>
-                            <li><a href="#">Item</a></li>
+                            <li><a href="#">door & window</a></li>
+                            <li><a href="#">kitchen</a></li>
+                            <li><a href="#">Tanda</a></li>
+                            <li><a href="#">library</a></li>
+                            <li><a href="#">Kitchen Hoods</a></li>
+                            <li><a href="#">Dressing Room</a></li>
+                            <li><a href="#">Bathroom sector</a></li>
+                            <li><a href="#">Modern tables</a></li>
+                            <li><a href="#">BUilt IN devices</a></li>
                         </ul>
                     </div>
                 </li>
             </ul>
         </div></div>
-        <a class="uk-navbar-item uk-logo" href="#">ElDar</a>
+        <a class="uk-navbar-item uk-logo " href="#">ElDar</a>
         <div class="uk-navbar-center-right uk-light uk-background-secondary"><div>
             <ul class="uk-navbar-nav">
                 <li><a href="#">contact</a></li>
@@ -80,10 +86,10 @@
     <div>
     <div class="uk-card uk-card-default">
             <div class="uk-card-media-top">
-                <img src="{!! asset('images/haustuer.jpg') !!}" alt="">
+                <img src="{!! asset('images/lg-appliances-package-awesome-like-sears-appliance-package-awesome-lg-kitchen-appliance-sets-buy-home-of-lg-appliances-package.jpg') !!}" alt="">
             </div>
             <div class="uk-card-body">
-                <h3 class="uk-card-title">Media Top</h3>
+                <h3 class="uk-card-title">kitchen devices</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
             </div>
         </div>
@@ -91,26 +97,63 @@
     <div>
     <div class="uk-card uk-card-default">
             <div class="uk-card-media-top">
-                <img src="{!! asset('images/haustuer.jpg') !!}" alt="">
+                <img src="{!! asset('images/71mwlDgJpfL._SX679_.jpg') !!}" alt="">
             </div>
             <div class="uk-card-body">
-                <h3 class="uk-card-title">Media Top</h3>
+                <h3 class="uk-card-title">Kitchen Hood</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
             </div>
         </div>
     </div>
-    <div>
-    <div class="uk-card uk-card-default">
-            <div class="uk-card-media-top">
-                <img src="{!! asset('images/haustuer.jpg') !!}" alt="">
+    
+    <div class="uk-margin-left">
+    <div class="uk-card uk-card-default uk-card-secondary">
+            <div class="uk-inline uk-card-media-top ">
+                <img class="uk-height-small" src="{!! asset('images/Baskets_3.jpg') !!}" alt="">
+                <p>Default Lorem ipsum</p>
+            
             </div>
-            <div class="uk-card-body">
-                <h3 class="uk-card-title">Media Top</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+    </div>
+    <div class="uk-card uk-card-default uk-card-secondary">
+            <div class="uk-card-media-top uk-inline">
+                <img class="uk-height-small" src="{!! asset('images/R3HP.jpg') !!}" alt="">
+                <p>Media Top</p>
             </div>
-        </div>
+    </div>
     </div>
 </div>
 </div>
+<div class="uk-height-large uk-background-cover uk-overflow-hidden uk-light uk-flex uk-flex-top uk-margin-top " style="background-image: url({!! asset('images/picture-of-modern-kitchen-design-dark-grey-floor-tiles-lovely.jpg') !!});">
+    <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical uk-light uk-overlay uk-overlay-primary" style="height:100%; width:100%">
+        <h1 uk-parallax="opacity: 0,1,1; y: -100,0,0; x: 100,100,0; scale: 2,1,1; viewport: 0.5;" style="margin-top:10%">Our vision</h1>
+        <p uk-parallax="opacity: 0,1,1; y: 100,0,0; x: -100,-100,0; scale: 0.5,1,1; viewport: 0.5;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
+</div>
+<div class="uk-grid" data-uk-grid-margin>
+
+
+                <div class="uk-width-medium-1-3">
+                    <div class="uk-panel uk-panel-box uk-panel-box-secondary">
+                        <h3 class="uk-panel-title">Contact Details</h3>
+                        <p>
+                            <strong>Company Name</strong>
+                            <br>Street, Country
+                            <br>Postal Zip Code
+                        </p>
+                        <p>
+                            <a>youremail@yourdomain.com</a>
+                            <br><a>@YourTwitterAccount</a><br>
+                            P+44 (0) 208 0000 000
+                        </p>
+                        <h3 class="uk-h4">Follow Us</h3>
+                        <p>
+                            <a href="#" class="uk-icon-button uk-icon-github"></a>
+                            <a href="#" class="uk-icon-button uk-icon-twitter"></a>
+                            <a href="#" class="uk-icon-button uk-icon-dribbble"></a>
+                            <a href="#" class="uk-icon-button uk-icon-html5"></a>
+                        </p>
+                    </div>
+                </div>
 @include('footer')
  
